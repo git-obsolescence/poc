9d1b74bc6d2b455477fd102f0f9581e869f3b93f
Carl Baldwin <carl@ecbaldwin.net>
2020-10-13 17:25:49 -0600

    fix verify commit

--- hooks/common.sh
+++ hooks/common.sh
@@ -1,3 +1,13 @@
+# verify_commit prints the sha1 of a valid commit and exits with 0. If the
+# commit is not valid in the current repository, it exits non-zero
+verify_commit() {
+    # git rev-parse only verifies that the ref derefenences to a valid sha1.
+    # It doesn't check that the commit is valid in the current repository.
+    # git rev-parse --quiet --verify $commit
+
+    git log -1 --pretty=%H $1 2>/dev/null
+}
+
 # The change id for any commit can be found by recursively traversing the first
 # out edge of the obsolescence graph until you get to a leaf commit. The sha of
 # the leaf commit is the change id.
@@ -15,18 +25,18 @@ get_change_id() {
 
     if [ -z "$first_edge" ]
     then
-        git rev-parse --quiet --verify $commit
+        verify_commit $commit
         return
     fi
 
-    if git rev-parse --quiet --verify ${commit}~ >/dev/null
+    if verify_commit ${commit}~ >/dev/null
     then
         first_parent_edge=$(git log -1 --pretty=%B ${commit}~ |
             git interpret-trailers --parse |
             awk '$1=="obsoletes:"{print$2;exit}')
         if [ "$first_edge" = "${first_parent_edge}" ]
         then
-            git rev-parse --quiet --verify $commit
+            verify_commit $commit
             return
         fi
     fi
